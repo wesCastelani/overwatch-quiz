9aed4b7bf7085b9796467e6a74efb77df7af0619
Wesley Castelani <wesleycastelani96@gmail.com>
2021-01-28 14:44:29 -0300

    criado a pagina de perguntas e os formularios iniciais com hooks

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,19 @@
 import styled from 'styled-components';
 import db from '../db.json';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+
 import Widget from '../src/components/Widget';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
 import QuizBackground from '../src/components/QuizBackground';
-
+import QuizLogo from '../src/components/QuizLogo';
+import { useState } from 'react';
 
 const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
-`
+`;
 
 // const BackgroundImage = styled.div`
 //   background-image: url(${db.bg});
@@ -29,25 +33,46 @@ const QuizContainer = styled.div`
   }
 `;
 
-
 export default function Home() {
-  return(
+  const router = useRouter();
+  const [name, setName] = useState('');
+
+  return (
     <QuizBackground backgroundImage={db.bg}>
       <QuizContainer>
-      <Widget>
-        <Widget.Header>
-          <h1>
-            Overwatch
-          </h1>
-        </Widget.Header>
-        <Widget.Content>
-          <p>blabla bla</p>
-        </Widget.Content>
-      </Widget>
-      <Widget><Widget.Content></Widget.Content></Widget>
-      <Footer />
-      </QuizContainer> 
-      <GitHubCorner projectUrl="https://github.com/wesCastelani"/>
+        <Head>
+          <title>Alura Quiz - Overwatch</title>
+        </Head>
+        <QuizLogo />
+        <Widget>
+          <Widget.Header>
+            <h1>Overwatch</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <form
+              onSubmit={function (infosDoEvento) {
+                infosDoEvento.preventDefault();
+                router.push(`/quiz?name=${name}`);
+              }}
+            >
+              <input
+                placeholder="Diz seu nome ai e bora la!"
+                onChange={function (infoDoEvento) {
+                  setName(infoDoEvento.target.value);
+                }}
+              />
+              <button type="submit" disabled={name.length === 0}>
+                Jogar {name}
+              </button>
+            </form>
+          </Widget.Content>
+        </Widget>
+        <Widget>
+          <Widget.Content></Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/wesCastelani" />
     </QuizBackground>
-  )
+  );
 }
